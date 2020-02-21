a97f868c8789aa46c2ec860d15b994e82e73c856
root <root@ip-172-31-40-121.ap-south-1.compute.internal>
2020-02-21 12:15:42 +0000

    adding the App.java commit

---
+++ prasadd/src/main/java/war/App.java
@@ -0,0 +1,13 @@
+package war;
+
+/**
+ * Hello world!how r u!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
